2776dcac8da4450ead2280166543f51bd314901b
Anthony van Beek <anthony@eventerprise.com>
2017-06-23 00:06:11 +0200

    changed to mail

--- submit.php
+++ submit.php
@@ -1,10 +1,10 @@
 <?php
 
 // if(isset($_POST['submit'])){
-    $to = "liquidspeed@gmail.com"; // this is your Email address
+    $to = "hello@anthonyvanbeek.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $name = $_POST['name'];
-    $subject = "Form submission from anthonyvanbeek.com from contact";
+    $subject = "Form submission from anthonyvanbeek.com";
     $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
     $headers = "From:" . $from;
     mail($to,$subject,$message,$headers);
